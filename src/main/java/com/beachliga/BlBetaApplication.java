5e687ab176e1e97b07e2b42a1b26c36ec4e928d6
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2017-02-17 12:44:47 +0100

    first commit

---
+++ src/main/java/com/beachliga/BlBetaApplication.java
@@ -0,0 +1,12 @@
+package com.beachliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlBetaApplication.class, args);
+	}
+}
