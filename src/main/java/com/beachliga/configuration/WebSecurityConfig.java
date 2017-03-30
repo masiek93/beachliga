a4d0906ddec914e059951bf3945448f0995d3224
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2017-03-30 20:14:31 +0200

    In Memory Authentication works.

--- src/main/java/com/beachliga/configuration/WebSecurityConfig.java
+++ src/main/java/com/beachliga/configuration/WebSecurityConfig.java
@@ -4,20 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/tournaments", "/tournaments/add").permitAll()
+				.antMatchers("/resources/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
-				.loginPage("/log_in")
+				//.loginPage("/log_in")
 				.permitAll()
 				.and()
 			.logout()
@@ -28,7 +31,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 			.inMemoryAuthentication()
-				.withUser("user@wp.pl").password("password").roles("USER");
+				.withUser("admin")
+					.password("password")
+					.roles("ADMIN","USER")
+				.and()
+				.withUser("user")
+					.password("password")
+					.roles("USER");
 	}
 
 }
